26d43c19bb057151dee3e096abc51819058f13a4
FabianoRF <freis884@gmail.com>
2020-09-30 14:52:02 -0300

    Projeto react configurado

---
+++ frontend/src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import SignIn from './pages/SignIn'
+
+const App: React.FC = () => {
+  return <SignIn />
+}
+
+export default App
---
+++ frontend/src/pages/SignUp/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+import { Container } from './styles'
+
+const SigUp: React.FC = () => {
+  return <Container>SigUp</Container>
+}
+
+export default SigUp
